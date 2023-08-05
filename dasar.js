0536c6742d400ccf05713227959a7a2e5d63f6bb
Maruli <tommypurba73@gmail.com>
2023-08-05 17:19:55 +0700

    test

---
+++ dasar.js
@@ -0,0 +1,64 @@
+//cara membuat varianbel;
+var a = 'a';
+let b = 'b';
+const v ='c';
+
+
+let aTas= "a";
+let Bawah= "test";
+let jess =  true;
+const uanh =10;
+
+// operator matematika
+
+let angka1=20;
+let angka2=30;
+console.log(angka1+ angka2);
+console.log(angka2-angka1);
+
+
+
+
+let PenampungHasil = angka1 - angka2;
+console.log (PenampungHasil, "hasil perkurang");
+PenampungHasil = angka2*angka1;
+console.log(PenampungHasil, "hasil perkalian");
+PenampungHasil = angka1+angka2;
+console.log(PenampungHasil, "hasil pertambahan");
+PenampungHasil = angka1/angka2;
+console.log(PenampungHasil, "hasil pembagian");
+
+
+
+// string untuk penambahan nama untuk pengurangan tidak bisa
+let fistName = "awe";
+let lastName = "yep";
+
+let fullName = fistName + lastName;
+
+let Fullname = fistName + " " + lastName;
+
+console.log(fistName + lastName);
+console.log(fistName + " " + lastName);
+console.log(fullName);
+console.log(Fullname);
+console.log(fullName-lastName)
+
+// alur pembacan code
+// dari kiri ke kanan dan dari atas ke bawa
+
+let number=2;
+console.log(number+23);
+number=11;
+console.log(number+3);
+number=false;
+console.log(number+1);
+number=true;
+console.log(number-2);
+
+// membaca error
+// error itu bagus, karena dalam perkerjaan kalau ada bug tanpa error , megakibat harus baca code dari awal
+
+// const hewan = "bebek";
+// console.log(hewan);
+// hewan="singa";
---
+++ index.js
@@ -0,0 +1,7 @@
+let a = prompt("masukkan baju mu");
+
+if (a<=3){
+    console.log("baju di prosees");
+} else {
+    console.log("kelebihan boy, maksimal 3 baju");
+}
