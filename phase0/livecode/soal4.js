8b95679f0f1afb673541b20ae40e760ac43fcdf0
Maruli <tommypurba73@gmail.com>
2023-08-20 11:36:53 +0700

    perbaiki folder

---
+++ phase0/livecode/soal4.js
@@ -0,0 +1,34 @@
+/*
+Count solo
+===========
+Diberikan sebuah function countSolo, dimana function ini akan menerima parameter sebagai array
+dan akan mereturn total jumlah dari angka angka yang TIDAK memiliki pasangan.
+Example:
+input: [ 5, 5, 6, 6, 3, 1, 2, 7, 7 ]
+proses: 3 + 1 + 2
+result: 6
+RULES:
+ - TIDAK BOLEH MENGGUNAKAN built in function .map, .filter, .reduce!
+ - TIDAK BOLEH MENGGUNAKAN built in function .indexOf dan .include!
+*/
+
+function countSolo(arr) {
+    let count = 0
+    for(let i = 0; i<arr.length;i++){
+        let hasil = false;
+        for(let j =0; j<arr.length;j++){
+            if(i !== j && arr[i]===arr[j]){
+                hasil = true;
+                break;
+            }
+        }
+        if(!hasil){
+            count += arr[i];
+        }
+    }
+    return count;
+}
+
+console.log(countSolo([5, 5, 6, 6, 3, 1, 2, 7, 7])) // 6
+console.log(countSolo([3, 6, 3, 6, 1, 1, 2, 1])) // 2
+console.log(countSolo([3, 3, 3, 3, 4, 5, 8, 10, 11])) // 38
---
+++ phase0/livecode/soal5.js
@@ -0,0 +1,55 @@
+/*
+=====================
+DOUBLE REVERSE ARRAY
+=====================
+
+[INSTRUCTION]
+Terdapat function doubleReverse yang menerima parameter berupa array of string, 
+function ini akan memutar elemen array, string yang berada di dalam elemen array juga akan dibalik atau diputar jika panjang string GENAP
+
+[EXAMPLE]
+input: ['rabu', 'cinta', 'benci', 'masuk', 'nikmat']
+proses: memutar isi array, dan memutar elemen array yang panjang katanya genap
+output: [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+
+[RULES]
+- Function bawaan javascript yang boleh digunakan hanyalah .push dan .length
+- DILARANG menggunakan .reverse()
+*/
+
+function doubleReverse(arr) {
+    
+    let arrBaru =[],arrNew =[]
+    
+    for(let i=0; i<arr.length;i++){
+        let nilai='';
+        if(arr[i].length %2===0){
+            
+            for(let j=arr[i].length-1; j>=0 ;j--){
+                nilai += arr[i][j];
+                
+            }arrBaru.push(nilai);
+            
+        }else{
+            arrBaru.push(arr[i]);
+        }        
+    }
+
+    for(let x=arrBaru.length-1;x>=0;x--){
+            arrNew.push(arrBaru[x]);
+    }
+    if(arrNew.length>0){
+    return (arrNew);}else{
+        return `invalid input parameter`;
+    }
+ 
+}
+
+console.log(doubleReverse(['rabu', 'cinta', 'benci', 'masuk', 'nikmat']));
+// [ 'tamkin', 'masuk', 'benci', 'cinta', 'ubar' ]
+console.log(doubleReverse(['aku', 'sayang', 'kamu']));
+// [ 'umak', 'gnayas', 'aku' ]
+console.log(doubleReverse(['pelakor', 'perusak', 'rumah', 'tangga']));
+//[ 'anggnat', 'rumah', 'perusak', 'pelakor' ]
+console.log(doubleReverse([]))
+// invalid input parameter
